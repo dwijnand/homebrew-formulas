d9ebcd90862848e4ae929affdc8c0411c4547ac6
Dale Wijnand <dale.wijnand@gmail.com>
2018-01-11 09:55:28 +0000

    jenv

---
+++ jenv.rb
@@ -0,0 +1,18 @@
+class Jenv < Formula
+  desc "Manage your Java environment"
+  homepage "http://www.jenv.be"
+  url "https://github.com/dwijnand/jenv/archive/0.4.5.tar.gz"
+  sha256 "a038ab91962c8a1b14391eb383fc6472468c56338526a406f486e9ff6c794dbc"
+  head "https://github.com/dwijnand/jenv.git"
+
+  bottle :unneeded
+
+  def install
+    libexec.install Dir["*"]
+    bin.write_exec_script libexec/"bin/jenv"
+  end
+
+  test do
+    shell_output("eval \"$(#{bin}/jenv init -)\" && jenv versions")
+  end
+end

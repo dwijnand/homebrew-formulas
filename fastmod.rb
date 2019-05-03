6ce48cc3b6153681e6bb254a565ba33a195c5553
Dale Wijnand <dale.wijnand@gmail.com>
2019-05-03 14:49:10 +0100

    fastmod 0.2.3

--- fastmod.rb
+++ fastmod.rb
@@ -1,8 +1,8 @@
 class Fastmod < Formula
   desc "A fast partial replacement for the codemod tool"
   homepage "https://github.com/facebookincubator/fastmod"
-  url "https://github.com/facebookincubator/fastmod/archive/v0.2.1.tar.gz"
-  sha256 "746b6b0f8e2b9a3fb2d2144a234ef30b6d88c6d38dcd499d30e644bb761d2593"
+  url "https://github.com/facebookincubator/fastmod/archive/v0.2.3.tar.gz"
+  sha256 "5ce4ffd9e09ed300dc2f24f7b4bb980c61085b1db5ba3b1d492338474c9cb0a9"
 
   depends_on "rust" => :build
 

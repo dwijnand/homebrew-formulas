1f4b714e604c45ecf2ef500ac595ec4232353cb4
Dale Wijnand <dale.wijnand@gmail.com>
2019-11-01 10:22:43 +0000

    ghc@7.10: Another start/quit attempt

--- ghc@7.10.rb
+++ ghc@7.10.rb
@@ -5,89 +5,12 @@ class GhcAT710 < Formula
 
   desc "Glorious Glasgow Haskell Compilation System"
   homepage "https://haskell.org/ghc/"
-  url "https://downloads.haskell.org/~ghc/7.10.3/ghc-7.10.3b-src.tar.bz2"
-  sha256 "b0bb177b8095de6074e5a3538e55fd1fc187dae6eb6ae36b05582c55f7d2db6f"
+  url "https://downloads.haskell.org/~ghc/7.10.3/ghc-7.10.3-x86_64-apple-darwin.tar.xz"
+  sha256 "852781d43d41cd55d02f818fe798bb4d1f7e52f488408167f413f7948cf1e7df"
 
   keg_only :versioned_formula
 
-  depends_on "python" => :build
-  depends_on "sphinx-doc" => :build
-
-  resource "gmp" do
-    url "https://ftp.gnu.org/gnu/gmp/gmp-6.1.2.tar.xz"
-    mirror "https://gmplib.org/download/gmp/gmp-6.1.2.tar.xz"
-    mirror "https://ftpmirror.gnu.org/gmp/gmp-6.1.2.tar.xz"
-    sha256 "87b565e89a9a684fe4ebeeddb8399dce2599f9c9049854ca8c0dfbdea0e21912"
-  end
-
-  # https://www.haskell.org/ghc/download_ghc_7_10_3.html#macosx_x86_64
-  # "This is a distribution for Mac OS X, 10.7 or later."
-  resource "binary" do
-    url "https://downloads.haskell.org/~ghc/7.10.3/ghc-7.10.3-x86_64-apple-darwin.tar.xz"
-    sha256 "852781d43d41cd55d02f818fe798bb4d1f7e52f488408167f413f7948cf1e7df"
-  end
-
-  resource "testsuite" do
-    url "https://downloads.haskell.org/~ghc/7.10.3/ghc-7.10.3-testsuite.tar.xz"
-    sha256 "50c151695c8099901334a8478713ee3bb895a90132e2b75d1493961eb8ec643a"
-  end
-
   def install
-    ENV["CC"] = ENV.cc
-    ENV["LD"] = "ld"
-
-    # Build a static gmp rather than in-tree gmp, otherwise all ghc-compiled
-    # executables link to Homebrew's GMP.
-    gmp = libexec/"integer-gmp"
-
-    # GMP *does not* use PIC by default without shared libs  so --with-pic
-    # is mandatory or else you'll get "illegal text relocs" errors.
-    resource("gmp").stage do
-      system "./configure", "--prefix=#{gmp}", "--with-pic", "--disable-shared",
-                            "--build=#{Hardware.oldest_cpu}-apple-darwin#{`uname -r`.to_i}"
-      system "make"
-      system "make", "check"
-      ENV.deparallelize { system "make", "install" }
-    end
-
-    args = ["--with-gmp-includes=#{gmp}/include",
-            "--with-gmp-libraries=#{gmp}/lib"]
-
-    # As of Xcode 7.3 (and the corresponding CLT) `nm` is a symlink to `llvm-nm`
-    # and the old `nm` is renamed `nm-classic`. Building with the new `nm`, a
-    # segfault occurs with the following error:
-    #   make[1]: * [compiler/stage2/dll-split.stamp] Segmentation fault: 11
-    # Upstream is aware of the issue and is recommending the use of nm-classic
-    # until Apple restores POSIX compliance:
-    # https://ghc.haskell.org/trac/ghc/ticket/11744
-    # https://ghc.haskell.org/trac/ghc/ticket/11823
-    # https://mail.haskell.org/pipermail/ghc-devs/2016-April/011862.html
-    # LLVM itself has already fixed the bug: llvm-mirror/llvm@ae7cf585
-    # rdar://25311883 and rdar://25299678
-    if DevelopmentTools.clang_build_version >= 703 && DevelopmentTools.clang_build_version < 800
-      args << "--with-nm=#{`xcrun --find nm-classic`.chomp}"
-    end
-
-    resource("binary").stage do
-      binary = buildpath/"binary"
-
-      system "./configure", "--prefix=#{binary}", *args
-      ENV.deparallelize { system "make", "install" }
-
-      ENV.prepend_path "PATH", binary/"bin"
-    end
-
-    system "./configure", "--prefix=#{prefix}", *args
-    system "make"
-
-    resource("testsuite").stage { buildpath.install Dir["*"] }
-    cd "testsuite" do
-      system "make", "clean"
-      system "make", "CLEANUP=1", "THREADS=#{ENV.make_jobs}", "fast"
-    end
-
-    ENV.deparallelize { system "make", "install" }
-    Dir.glob(lib/"*/package.conf.d/package.cache") { |f| rm f }
   end
 
   def post_install

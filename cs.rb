6430f806f6d3e491787debdf87ac0bbc1de16da3
Dale Wijnand <dale.wijnand@gmail.com>
2020-01-29 16:16:07 +0000

    Add alternative coursier formula

---
+++ cs.rb
@@ -0,0 +1,21 @@
+# https://docs.brew.sh/Formula-Cookbook
+# https://rubydoc.brew.sh/Formula
+class Cs < Formula
+  desc "Pure Scala Artifact Fetching"
+  homepage "https://get-coursier.io"
+  url "https://github.com/coursier/coursier/releases/download/v2.0.0-RC5-6/cs-x86_64-apple-darwin"
+  version "2.0.0-RC5-6"
+  sha256 "c5010aa0e2ed7bbd132196f10f62edd8d3fd4415bc324ed982526f55e33af974"
+  bottle :unneeded
+
+  def install
+    mv "cs-x86_64-apple-darwin", "cs"
+    bin.install "cs"
+  end
+
+  test do
+    ENV["COURSIER_CACHE"] = "#{testpath}/cache"
+    output = shell_output("#{bin}/cs launch io.get-coursier:echo:1.0.2 -- foo")
+    assert_equal ["foo\n"], output.lines
+  end
+end

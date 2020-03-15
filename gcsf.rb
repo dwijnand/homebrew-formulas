b7327f3d004a3f48c039b9798eb76dbd433ce7b7
Dale Wijnand <dale.wijnand@gmail.com>
2020-03-15 21:11:46 +0000

    wip gcsf

---
+++ gcsf.rb
@@ -0,0 +1,8 @@
+class Gcsf < Formula
+  desc "a FUSE file system based on Google Drive"
+  url "https://github.com/harababurel/gcsf/releases/download/0.1.25/gcsf-0.1.25-x86_64-apple-darwin.tar.gz"
+  sha256 "0ef9e54230669aaeb7218a95f610df2f8c610232a8f233df81cbb866cae6ff74"
+
+  def install
+  end
+end

7c60b9933eed2a958183550a48e0f1c52d43a31c
Dale Wijnand <dale.wijnand@gmail.com>
2021-01-29 10:01:39 +0000

    Use sbt-extras' HEAD date as sbtx version

--- sbtx.rb
+++ sbtx.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Sbtx < Formula
   homepage "https://github.com/paulp/sbt-extras"
   url "https://raw.githubusercontent.com/paulp/sbt-extras/4db8d5c27413f69297adfffac57485d88d73c60e/sbt"
-  version "20190913"
+  version "20210129"
   sha256 "25b8177ed65d6a8d90c58c229befad26096a64e0f6d9c07b6bddaa7cc58a0966"
   head "https://git.io/sbt", :using => :curl
 

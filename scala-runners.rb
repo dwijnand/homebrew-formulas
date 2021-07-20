8aae150bfe47bab4f9cbcfee368cb6b92431c815
Dale Wijnand <dale.wijnand@gmail.com>
2021-07-20 19:23:38 +0100

    Change branch to main

--- scala-runners.rb
+++ scala-runners.rb
@@ -1,7 +1,7 @@
 class ScalaRunners < Formula
   desc "Coursier-based alternative Scala runners"
   homepage "https://github.com/dwijnand/scala-runners"
-  head 'git://github.com/dwijnand/scala-runners.git'
+  head 'git://github.com/dwijnand/scala-runners.git', :branch => "main"
 
   depends_on "coursier/formulas/coursier"
 
